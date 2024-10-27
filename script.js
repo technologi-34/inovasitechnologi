c0899b751547dec9e4d30363e95241afc080d69d
technologi-34 <technologi497@gmail.com>
2024-10-27 18:03:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-document.getElementById('learn-more-btn').addEventListener('click', function() {
-    document.querySelector('.hero').scrollIntoView({ behavior: 'smooth' });
-});
+
 function toggleMenu() {
   const menu= document.querySelector('.menu');
   const menu= document.querySelector('.menu-toggle');
